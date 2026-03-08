1a3c88b05b1657cc90ee0cc8d4c92d89d9f6b58e
smotorol <smotorol@gmail.com>
2026-03-08 15:34:29 +0900

    db 생성

--- dockerredis/init/00_create_db.sql
+++ dockerredis/init/00_create_db.sql
@@ -2,7 +2,9 @@
 
 IF DB_ID(N'$(DBNAME)') IS NULL
 BEGIN
-    DECLARE @sql NVARCHAR(MAX) = N'CREATE DATABASE [' + REPLACE(N'$(DBNAME)', N']', N']]') + N'];';
+    DECLARE @sql NVARCHAR(MAX) =
+        N'CREATE DATABASE [' + REPLACE(N'$(DBNAME)', N']', N']]') +
+        N'] COLLATE Korean_100_CI_AS_SC_UTF8;';
     EXEC (@sql);
 END
 GO
