9fb26d7edcb2dc62ce6f7866740aa090550d3d90
smotorol <smotorol@gmail.com>
2026-03-23 11:26:50 +0900

    update

---
+++ runtime_smoke.bat
@@ -0,0 +1,3 @@
+set WORLD_SERVER_BIN=G:\Programing\Work\new_server\Bin\Debug\world_server_d.exe
+set WORLD_SERVER_CWD=G:\Programing\Work\new_server\Bin\Debug
+python tests\runtime_smoke_world_shutdown.py
